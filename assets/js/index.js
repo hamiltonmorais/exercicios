2bfcc80fbdeeebdd97147b77ac8ef638cef1a1ed
Hamilton Morais <92332979+hamiltonmorais@users.noreply.github.com>
2022-03-03 09:27:50 -0300

    Armazenando informações em array

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+
+function meuEscopo (){
+  const form = document.querySelector('.form');
+  const resultado = document.querySelector('.resultado');
+
+  const pessoas = [];
+  function recebeEventoForm(evento) {
+    evento.preventDefault();
+    const nome = form.querySelector('.nome');
+    const sobrenome = form.querySelector('.sobrenome');
+    const idade = form.querySelector('.idade');
+    const peso = form.querySelector('.peso');
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      idade: idade.value,
+      peso: peso.value
+    })
+    console.log(pessoas);
+    
+    resultado.innerHTML+= `<p> Nome: ${nome.value} <br> Sobrenome: ${sobrenome.value} <br> Idade: ${idade.value} <br> Peso: ${peso.value} </p>`
+  }
+  form.addEventListener('submit', recebeEventoForm);
+}
+meuEscopo();
